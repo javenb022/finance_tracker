525a07bfab314248b217e6e5a4e7ee230d90c121
Javen Wilson <javenb022@gmail.com>
2024-08-27 22:16:10 -0400

    Refactored enum for rubocop

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,5 +5,5 @@ class Transaction < ApplicationRecord
 
   validates :amount, :transaction_type, :transaction_date, presence: true
 
-  enum transaction_type: %i[income expense transfer]
+  enum transaction_type: { income: 0, expense: 1, transfer: 2 }
 end
