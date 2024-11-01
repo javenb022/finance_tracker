196b80410503c493da95cb723ab7e67b18953550
Javen Wilson <javenb022@gmail.com>
2024-11-01 11:21:15 -0400

    Update factory bot data to include better sample data

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :account do
-    name { Faker::Company.name }
+    name { ["Checking", "Savings", "Credit Card"].sample }
     balance { Faker::Number.decimal(l_digits: 2) }
     account_type { rand(0..2) }
     user
--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction do
     amount { 100.00 }
-    description { Faker::Lorem.sentence }
+    description { ["Rent", "Costco", "Amazon Purchase", "Utilities", "Birthday Money", "Texas Roadhouse", "Walmart", "Kroger", "Paycheck", "Ebay"].sample }
     # category { ["income", "expense", "transfer"].sample }
     transaction_date { Time.zone.today }
     transaction_type { rand(0..2) }
