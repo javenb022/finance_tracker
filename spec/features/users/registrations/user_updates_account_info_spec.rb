722b3a995387a8d39c291405bcc228dc4cbe11e6
Javen Wilson <javenb022@gmail.com>
2024-11-04 23:11:06 -0500

    Fixed tests to redirect to profile_path, fixed field names

--- spec/features/users/registrations/user_updates_account_info_spec.rb
+++ spec/features/users/registrations/user_updates_account_info_spec.rb
@@ -21,7 +21,7 @@ RSpec.feature 'User updates account information' do
       expect(page).to have_field('user_city', with: user.city)
       expect(page).to have_field('user_state', with: user.state)
       expect(page).to have_field('user_zip_code', with: user.zip_code)
-      expect(page).to have_field('user_date_of_birth', with: user.date_of_birth)
+      expect(page).to have_field('user[date_of_birth]', with: user.date_of_birth)
     end
 
     scenario 'user updates their first name' do
@@ -29,7 +29,7 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.first_name).to eq('John')
     end
@@ -39,7 +39,7 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.last_name).to eq('Doe')
     end
@@ -49,7 +49,7 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.email).to eq('new_email@example.com')
     end
@@ -60,7 +60,7 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
 
       click_button 'Update'
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.valid_password?('new_password')).to be true
     end
@@ -70,9 +70,9 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
-      expect(user.reload.phone_number).to eq('987-654-3210')
+      expect(user.reload.phone_number).to eq('(987) 654-3210')
     end
 
     scenario 'user updates their address' do
@@ -80,7 +80,7 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.address).to eq('456 Elm St')
     end
@@ -90,17 +90,17 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.city).to eq('Othertown')
     end
 
     scenario 'user updates their state' do
-      fill_in 'user_state', with: 'CA'
+      select 'California', from: 'user_state'
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.state).to eq('CA')
     end
@@ -110,17 +110,17 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.zip_code).to eq('54321')
     end
 
     scenario 'user updates their date of birth' do
-      fill_in 'user_date_of_birth', with: '2000-01-01'
+      fill_in 'user[date_of_birth]', with: '2000-01-01'
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.date_of_birth.to_s).to eq('2000-01-01')
     end
@@ -132,7 +132,7 @@ RSpec.feature 'User updates account information' do
       fill_in 'user_current_password', with: user.password
       click_button 'Update'
 
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(profile_path)
       expect(page).to have_content('Your account has been updated successfully.')
       expect(user.reload.first_name).to eq('John')
       expect(user.reload.last_name).to eq('Doe')
