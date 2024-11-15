1f61b9cc5677358dcdfa56e4d037b24a7c17a4b6
Javen Wilson <javenb022@gmail.com>
2024-11-14 19:40:19 -0500

    method to set category id based on transaction id

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,4 +6,11 @@ class Transaction < ApplicationRecord
   validates :amount, :transaction_type, :transaction_date, presence: true
 
   enum transaction_type: { income: 0, expense: 1, transfer: 2 }
+
+  def set_default_category
+    self.category_id ||= {
+      "income" => 1,
+      "expense" => 2
+    }.fetch(transaction_type, 0)
+  end
 end
